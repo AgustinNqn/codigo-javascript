3f46cacc9b3c99b4b027733629d74ca353310a5f
AgustinNqn <eze.vela72@gmail.com>
2024-02-07 17:45:18 -0300

    Terminado general

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,70 @@
 console.log("Inicio del programa");
 
+const btnAdd = document.querySelectorAll('.btnCompra');
 const jordans = [
-    { nombre: "Jordan 1", precio: 50000},
-    { nombre: "Jordan 2", precio: 50000},
-    { nombre: "Jordan 3", precio: 50000} 
+    { nombre: "Jordan 1", precio: 250000, enviosPais: true},
+    { nombre: "Jordan 2", precio: 200000, enviosPais: true},
+    { nombre: "Jordan 3", precio: 350000, enviosPais: true},
+    { nombre: "Jordan 4", precio: 200000, enviosPais: true},
+    { nombre: "Jordan 5", precio: 300000, enviosPais: true},
+    { nombre: "Jordan 6", precio: 150000, enviosPais: true},
+    { nombre: "Jordan 7", precio: 250000, enviosPais: true},
+    { nombre: "Nike 1", precio: 125000, enviosPais: true}
 ];
-console.log("Cantidad de Jordans en el array:", jordans.length);
 
-let nombreUser = prompt("Cual es tu nombre?");
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+console.log(carrito)
+const jordansList = document.getElementById('jordans-list');
+
+
+
+btnAdd.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        Swal.fire({
+            title: '¿Deseas agregar el producto al carrito?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Sí, agregar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                carrito.push(jordans[index]);
+                localStorage.setItem('carrito', JSON.stringify(carrito));
+                Swal.fire('¡Agregado al carrito!', 'El producto ha sido agregado al carrito exitosamente.', 'success');
+                actualizarCantidadZapatillas();
+                mostrarCarrito();
+            } else {
+                Swal.fire('Operación cancelada', 'El producto no fue agregado al carrito.', 'info');
+            }
+        });
+    });
+});
+function actualizarCantidadZapatillas() {
+    const cantidadZapatillas = document.getElementById('cantidad-zapatillas');
+    cantidadZapatillas.textContent = carrito.length.toString();
+}
+
+actualizarCantidadZapatillas();
+
+
+jordans.forEach(jordan => {
+    const card = document.createElement('div');
+    card.classList.add('jordans-cards');
+    card.innerHTML = `
+        <h2>${jordan.nombre}</h2>
+        <img class="img-jordans" src="${jordan.imagen}" alt="${jordan.nombre}">
+        <div class="jordans-info">
+            <p>Precio: $${jordan.precio}</p>
+            <p>Stock: ${jordan.stock}</p>
+        </div>
+    `;
+    jordansList.appendChild(card);
+});
+
+
+console.log("Cantidad de Zapatillas en el array:", jordans.length);
+
+let nombreUser = localStorage.getItem('nombreUser') || prompt("Cual es tu nombre?");
 console.log("Nombre del usuario:", nombreUser);
 
 function inicio(nombre) {
@@ -62,10 +119,12 @@ function jordanFakeTrue(jordans) {
 
         let seguirComprando = prompt("¿Quieres seguir comprando? (si/no)").toLowerCase();
         if (seguirComprando !== 'si') {
-            break; // Salir del bucle si el usuario no quiere seguir comprando
+            break; 
         }
     } while (true);
 }
+
+localStorage.setItem('nombreUser', nombreUser);
 
 inicio(nombreUser);
 jordanFakeTrue(jordans);
